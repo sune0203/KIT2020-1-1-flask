ac65e6565219b3b525901a62692dd0a5c492db9b
lee su jong <sune0203.naver.com>
2020-06-22 17:30:11 +0900

    10주차

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, redirect,url_for,abort
 
 import game
+import json
 
 app = Flask(__name__)
 
@@ -15,15 +16,27 @@ def hello():
 @app.route('/hello/<name>')
 def hellovar(name):
     character = game.set_charact(name)
-    return "{0}님 반갑습니다. (HP {1})으로 게임을 시작 합니다".format(character["name"], character["hp"])
+    return render_template('gamestart.html',data=character)
+
+@app.route('/gamestart')
+def gamestart():
+    with open("static/save.txt", "r", encoding='utf-8') as f:
+        data = f.read()
+        character = json.loads(data)
+        print(character['items'])
+    return "{} 이 {}아이템을 사용 해서 이겼다.".format(character["name"], character["items"][0])
 
 @app.route('/input/<int:num>')
 def input_name(num):
     if num == 1:
-        return "도라에몽"
-    elif num == 2:
-        return "진구"
-    elif num == 3:
+        with open("static/save.txt", "r", encoding='utf-8') as f:
+            data = f.read()
+            character = json.loads(data)
+            print(character['items'])
+        return "{} 이 {}아이템을 사용 해서 이겼다.".format(character["name"], character["items"][0])
+    elif num ==2:
+        return "도망갔다."
+    elif num ==3:
         return "퉁퉁이"
     else:
         return "없어요"
@@ -56,7 +69,17 @@ def method():
         num = request.form['num']
         name = request.form['name']
         print(num, name)
+        with open("static/save.txt","w", encoding='utf-8') as f:
+            f.write("%s,%s" % (num, name))
         return 'POST 이다. 학번은: {} 이름은: {}'.format(num,name)
+
+
+@app.route('/getinfo')
+def getinfo(): 
+# 파일 입력 
+    with open("static/save.txt", "r", encoding='utf-8') as file: 
+        student = file.read().split(',') # 쉽표로 잘라서 student 에 배열로 저장 
+        return '번호 : {}, 이름 : {}'.format(student[0], student[1])
 
 
 
@@ -90,6 +113,4 @@ def img():
  return render_template("image.html")
 
 if __name__ == '__main__':
-    with app.test_request_context():
-        print(url_for('daum'))
     app.run(debug=True)
--- game.py
+++ game.py
@@ -1,17 +1,24 @@
+def aa(name):
+    return name
+
+import json
+
 def set_charact(name):
     character = {
         "name": name,
         "level": 1,
         "hp": 100,
-        "items":["체력포션", "마나포션", "뇌물"],
-        "skill":["주먹질","도발","발차기"]
+        "items": ["대나무헬리콥터", "빅라이트", "어디로든 문"],
+        "skill": ["펀치", "핵펀치", "피하기"]
     }
-    print("{0}님 반갑습니다. (HP {1})으로 게임을 시작 합니다".format(character["name"], character["hp"]))
+    with open("static/save.txt", "w", encoding='utf-8') as f:
+        json.dump(character, f, ensure_ascii= False, indent=4) 
+    print("님 반갑습니다.", "(HP {1})으로 게임을 합니다." .format(character["name"], character["hp"]))
     return character
 
 def save_game(filename, charact):
-    f = open("save.txt", "w", encoding="utf-8")
+    f = open(filename, "w", encoding="utf-8")
     for key in charact:
         print("%s:%s" % (key, charact[key]))
-        f.write("%s:%s\n" % (key, charact[key]))
+        f.write("%s:%s\n" % (key,charact[key]))
     f.close()
